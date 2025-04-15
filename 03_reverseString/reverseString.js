ca66a42f59c6e6b58d2cb725cf66760fd1a50302
zxdinay <deepcut.atm@gmail.com>
2025-04-15 08:54:38 +0800

    Added a solution to the 03 reversestring exercises in TDD

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,10 @@
-const reverseString = function() {
+const reverseString = function (word) {
+    return word.split('').reverse().join('');
+    /* let reverseWord = "";
+    for (let i = word.length - 1; i >= 0; i--) {
+        reverseWord += word[i];
+    }
+    return reverseWord; */
 
 };
 
