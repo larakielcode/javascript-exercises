9d27aedc1d61903839b83b17b210445565ed7431
zxdinay <deepcut.atm@gmail.com>
2025-04-15 08:34:44 +0800

    02 repeat strin tdd solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    let word = '';
+    if (num < 0) return "ERROR";
+    while (num) {
+        word = word + string;
+        num--;
+    }
 
+    return word;
 };
+
+repeatString('hey', 3);
+repeatString('hello', 10);
+repeatString('hi', 1);
+repeatString('bye', 0);
+repeatString('goodbye', -1);
 
 // Do not edit below this line
 module.exports = repeatString;
