f97ba9241947aea0e9bd7af8cc3bdc8957f56015
ezekielmyson <deepcut.atm@gmail.com>
2025-04-16 21:31:16 +0800

    Added js solution to 06 leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function (year) {
+   /* if ((year % 4 === 0) && (!(year % 100 === 0) || year % 400 === 0)) {
+      return true;
+   } else { return false; } */
 
+   const isYearDivisibleByFour = year % 4 === 0;
+   const isYearCenturyYear = year % 100 === 0;
+   const isYearDivisibleByFourH = year % 400 === 0;
+
+   return (isYearDivisibleByFour && (!isYearCenturyYear || isYearDivisibleByFourH)) ? true : false;
 };
 
 // Do not edit below this line
