2d3810e32e86ee8bfe156312f2b79290b90fd17a
ezekielmyson <deepcut.atm@gmail.com>
2025-04-16 20:31:55 +0800

    Added js solution to 04 removefromarray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+   return array.filter(val => !args.includes(val));
+
+   /* const newArr = [];
+
+   array.forEach((val) => {
+      if (!args.includes(val)) {
+         newArr.push(val);
+      }
+   });
+   return newArr; */
 };
 
 // Do not edit below this line
