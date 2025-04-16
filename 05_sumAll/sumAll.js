d5df4892f52384029a8382cb9aca5dd6bb5f00a4
ezekielmyson <deepcut.atm@gmail.com>
2025-04-16 20:55:04 +0800

    Added js solution to 05 sumAll exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function (numOne, numTwo) {
+   if (numOne < 0 || numTwo < 0) return "ERROR";
+   if (!Number.isInteger(numOne) || !Number.isInteger(numTwo)) return "ERROR";
+   if (numOne > numTwo) {
+      const tempNum = numOne;
+      numOne = numTwo;
+      numTwo = tempNum;
+   }
+   let sum = 0;
+   for (let x = numOne; x <= numTwo; x++) {
+      sum += x;
+   }
+   return sum;
 };
 
 // Do not edit below this line
