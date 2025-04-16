5d5e76933af7641a371ef3bd4abef671e736df11
ezekielmyson <deepcut.atm@gmail.com>
2025-04-16 21:49:49 +0800

    Added js solution to 07 temperature conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  return Math.round((temperature - 32) * (5 / 9) * 10) / 10;
+  /* Deduct 32, then multiply by 5, then divide by 9 */
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  return Math.round((((temperature / 5) * 9) + 32) * 10) / 10;
+  /* Divide by 5, then multiply by 9, then add 32 */
 };
 
 // Do not edit below this line
