a76b99296092677ea28fa1a3ebe90ba567c6836f
zxdinay <deepcut.atm@gmail.com>
2025-04-27 23:32:12 +0800

    Added an answer to fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (numArgs) {
+
+    let counter
+    if (typeof numArgs !== Number) {
+        counter = parseInt(numArgs);
+    } else {
+        counter = numArgs;
+    }
+
+    if (counter < 0) return "OOPS";
+    if (counter == 0) return 0;
+
+    let first = 1;
+    let second = 0;
+    for (let index = 2; index <= counter; index++) {
+        let current = first + second;
+        second = first;
+        first = current;
+    }
+    return first;
 
 };
 
