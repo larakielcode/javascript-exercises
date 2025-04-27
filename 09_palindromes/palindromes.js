660806ca3c7792e7d167670d01f80b06a71435b7
zxdinay <deepcut.atm@gmail.com>
2025-04-27 23:07:45 +0800

    added an answer to 09 palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (text) {
 
+    const alphaNumeric = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+    const cleanedString = text
+        .toLowerCase()
+        .split('')
+        .filter((character) => alphaNumeric.includes(character))
+        .join();
+
+    const reverseString = cleanedString.split('').reverse().join('');
+    return cleanedString === reverseString;
 };
 
 // Do not edit below this line
